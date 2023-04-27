cd549606e1cc92d705f924aaf2633219761f8d4b
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-27 15:12:05 +0530

    Optimised the cart for the items adding and removing items

--- src/store/data-context.js
+++ src/store/data-context.js
@@ -2,26 +2,38 @@ import React, { useState } from "react";
 
 export const DataContextProvider = (props) => {
   const [noOfItems, setNoOfItems] = useState(0);
-  const [cartitemlist, setCartItemList] = useState([
-    { name: "Cart is Empty", price: 0, amount: 0 },
-  ]);
+  const [cartitemlist, setCartItemList] = useState([]);
+  const [inCartList, setCartItemId] = useState([]);
   const onAdd = (item) => {
-    if (noOfItems === 0) {
-      setCartItemList([item]);
+    if (inCartList.indexOf(item.id) !== -1) {
+      const list = [...cartitemlist];
+      list[inCartList.indexOf(item.id)].amount += parseInt(item.amount);
+      setCartItemList(list);
     } else {
+      setCartItemId([...inCartList, item.id]);
       setCartItemList([...cartitemlist, item]);
     }
     setNoOfItems(noOfItems + parseInt(item.amount));
   };
   const removeItem = (action, id) => {
     const precartitem = [...cartitemlist];
-
+    const preInCart = [...inCartList];
     if (action === "add") {
       precartitem[id].amount += 1;
       setCartItemList(precartitem);
+      setNoOfItems((noOfItems) => noOfItems + 1);
     } else {
-      precartitem[id].amount -= 1;
-      setCartItemList(precartitem);
+      if (precartitem[id].amount === 1) {
+        preInCart.splice(id, 1);
+        precartitem.splice(id, 1);
+        setCartItemId(preInCart);
+        setCartItemList(precartitem);
+        setNoOfItems((noOfItems) => noOfItems - 1);
+      } else {
+        precartitem[id].amount -= 1;
+        setCartItemList(precartitem);
+        setNoOfItems((noOfItems) => noOfItems - 1);
+      }
     }
   };
   return (
